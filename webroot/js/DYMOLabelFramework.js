cc084e655160f51563ba1c705e4ccf0c478487cd
Jose Guardiola <jguardiola9510@gmail.com>
2016-12-28 09:12:47 -0600

    DYMO framework, need testing

---
+++ webroot/js/DYMOLabelFramework.js
@@ -0,0 +1,2 @@
+<script src="http://labelwriter.com/software/dls/sdk/js/DYMO.Label.Framework.latest.js"
+        type="text/javascript" charset="UTF-8"> </script>
---
+++ webroot/js/print.js
@@ -0,0 +1,16 @@
+var labelXml = '';
+var label = dymo.label.framework.openLabelXml(labelXml);
+var printers = dymo.label.framework.getPrinters();
+if (printers.length == 0)
+    throw "No DYMO printers are installed. Install DYMO printers.";
+
+var printerName = "";
+for (var i = 0; i < printers.length; ++i)
+{
+    var printer = printers[i];
+    if (printer.printerType == "LabelWriterPrinter")
+    {
+        printerName = printer.name;
+        break;
+    }
+}
